c24566c4a99f18fc4af44bb3bcf2c73de038160b
yamamoto0901 <kamui9610.yy@gmail.com>
2020-11-04 19:22:41 +0900

    問題作成

---
+++ app.js
@@ -0,0 +1,10 @@
+const question ='ファイナルファンタジー７の主人公は誰？';
+const answers = [
+    'ティーダ',
+    'スコール',
+    'クラウド',
+    'ヴァン'
+];
+const correct = 'クラウド';
+
+console.log(document.getElementById('js-questin').textContent = question);

8bc9f020ed915e8544395c1001b9d4e3e2d1b12e
yamamoto0901 <kamui9610.yy@gmail.com>
2020-11-04 21:49:33 +0900

    正誤判定

--- app.js
+++ app.js
@@ -8,3 +8,45 @@ const answers = [
 const correct = 'クラウド';
 
 console.log(document.getElementById('js-questin').textContent = question);
+
+const $button = document.getElementsByTagName('button')
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+
+
+$button[0].addEventListener('click', () => {
+    if(correct === document.getElementsByTagName('button')[0].textContent){
+        window.alert('正解');
+    }
+    else{
+    window.alert('不正解');
+    }
+});
+$button[1].addEventListener('click', () => {
+    if(correct === document.getElementsByTagName('button')[1].textContent){
+        window.alert('正解');
+    }
+    else{
+    window.alert('不正解');
+    }
+});
+$button[2].addEventListener('click', () => {
+    if(correct === document.getElementsByTagName('button')[2].textContent){
+        window.alert('正解');
+    }
+    else{
+    window.alert('不正解');
+    }
+});
+$button[3].addEventListener('click', () => {
+    if(correct === document.getElementsByTagName('button')[3].textContent){
+        window.alert('正解');
+    }
+    else{
+    window.alert('不正解');
+    }
+});
